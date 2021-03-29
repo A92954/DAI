3b842a92e67f73a4e0134db494426f3305419df8
A92954 <a92954@alunos.uminho.pt>
2021-03-29 22:52:11 +0100

    Design 90% Fim

--- app/src/main/java/com/example/dai/History.java
+++ app/src/main/java/com/example/dai/History.java
@@ -1,11 +1,15 @@
 package com.example.dai;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -32,12 +36,19 @@ public class History extends AppCompatActivity {
         });
 
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public void showPopup(View v){
         TextView txtclose;
         Button btn;
         myDialog.setContentView(R.layout.popup_acti);
         btn = (Button) myDialog.findViewById(R.id.btn);
         myDialog.show();
+        Display display =((WindowManager)getSystemService(History.WINDOW_SERVICE)).getDefaultDisplay();
+        int width = display.getWidth();
+        int height=display.getHeight();
+
+        myDialog.getWindow().setLayout((6*width + 1)/7,(4*height + 1)/5);
     }
 
 }
