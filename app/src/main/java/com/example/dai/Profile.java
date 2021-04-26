09a5c992d8c66cd3c33689931878bdfc56c76406
Ricardo <ricardomagalhaes168@gmail.com>
2021-04-26 09:29:25 +0100

    profile

--- app/src/main/java/com/example/dai/Profile.java
+++ app/src/main/java/com/example/dai/Profile.java
@@ -44,9 +44,9 @@ public class Profile extends AppCompatActivity {
 
         int id_child = session.getID_CHILD();
 
-        client = new AsyncHttpClient();
+        //client = new AsyncHttpClient();
 
-        String URL = "http://93.108.170.117:8080/DAI-end/profile1?id_child=" +id_child;
+        //String URL = "http://93.108.170.117:8080/DAI-end/profile1?id_child=" +id_child;
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,11 +58,11 @@ public class Profile extends AppCompatActivity {
                 params.put("child_name", n);
                 params.put("address", m);
 
-                //client = new AsyncHttpClient();
+                client = new AsyncHttpClient();
 
                 //Ir buscar a session
 
-                //String URL = "http://93.108.170.117:8080/DAI-end/profile1?id_child=1";
+                String URL = "http://93.108.170.117:8080/DAI-end/profile1?id_child=1";
 
                 client.put(URL, params, new JsonHttpResponseHandler() {
                     @Override
